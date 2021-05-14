6c08100124b01848b8149820197d57255eba6dfc
Oliver Graml <o.graml@gmx.de>
2021-05-14 17:16:33 +0200

    refreshed some settings

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,18 @@
 import styled from 'styled-components/macro';
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from 'react';
 
- function Home() {
+import App from './App';
 
-
-    return (
+function Home() {
+  return (
     <div>
-        <Headline> Pokemon React App</Headline>
-        <section>
+      <Headline> Pokemon React App</Headline>
+      <section>
         <Home />
         <App />
-    </section>
+      </section>
     </div>
-    )
+  );
 }
 
 export default Home;
@@ -25,7 +25,6 @@ const Headline = styled.h1`
   padding: 1rem;
   border-radius: 3rem;
 `;
-
 
 const Button = styled.button`
   background-image: linear-gradient(#ff0f7b, #f89b29);
